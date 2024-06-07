e4b972f7eec63f607dcf8720b35c9b287384e4ed
sheraz4196 <sheraztoor321@gmail.com>
2024-06-08 02:29:35 +0500

    feat: initial commit with defined login form

---
+++ src/components/forms/login-form.tsx
@@ -0,0 +1,25 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "../ui/form";
+
+const loginFormSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." }),
+});
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form></form>
+    </Form>
+  );
+}
