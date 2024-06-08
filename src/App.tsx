75c1c26ed6a2944f58cd009d597e1a512fa85880
sheraz4196 <sheraztoor321@gmail.com>
2024-06-09 01:07:39 +0500

    feat: inbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages";
+import Inbox from "./pages/inbox";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/register" element={<Homepage />} />
+      <Route path="/index" element={<Inbox />} />
     </Routes>
   </Router>
 );
