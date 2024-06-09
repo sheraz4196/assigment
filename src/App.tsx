eafc1bef45f8a4fb9e42099bb6df11a437aea0b9
sheraz4196 <sheraztoor321@gmail.com>
2024-06-09 20:45:38 +0500

    fix: issue of register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages";
 import Inbox from "./pages/inbox";
 import { Toaster } from "./components/ui/sonner";
+import RegisterPage from "./pages/register-page";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/register" element={<Homepage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/inbox" element={<Inbox />} />
     </Routes>
     <Toaster theme="system" richColors toastOptions={{}} />
--- src/pages/register-page.tsx
+++ src/pages/register-page.tsx
@@ -1,12 +1,12 @@
 import Poster from "@/components/general/poster";
-import LoginForm from "@/components/forms/login-form";
-function Homepage() {
+import RegisterForm from "@/components/forms/register-form";
+function RegisterPage() {
   return (
     <section className="flex h-screen max-h-screen w-screen">
-      <LoginForm />
+      <RegisterForm />
       <Poster />
     </section>
   );
 }
 
-export default Homepage;
+export default RegisterPage;
